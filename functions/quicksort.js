bd5b108118678b797f35dcb6eb95bb7a73d23401
XCode <64787166+1337Xcode@users.noreply.github.com>
2024-10-23 01:58:23 +0100

    Update quicksort.js

--- functions/quicksort.js
+++ functions/quicksort.js
@@ -1,94 +1,36 @@
+let quickSort = {
+    sort: (array, animation, l=0, r) => {
+        if (r===undefined) r=array.length-1
+        if (l>=r) return //base case
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
+        let pivot = quickSort.partition(array, animation, l, r)
+        quickSort.sort(array, animation, l, pivot-1)
+        quickSort.sort(array, animation, pivot+1, r)
+    },
 
-    if (left < right) {
-        let pivotIdx = partition(arr, left, right);
-        quickSort(arr, left, pivotIdx - 1);
-        quickSort(arr, pivotIdx + 1, right);
-    }
+    partition: (array, animation, l, r) => {
+        let pivot = r
+        let i = l-1
+        for (let j=l; j<r; j++) {
+            if (array[j]<array[pivot]) {
+                i++
+                let temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+                animation.push({indices:[pivot, i, j], numbers: [array[i], array[j]],sorted: i===j})
 
-    return arr;
-}
-
-function partition(arr, start, end) {
-    
-    let pivot = arr[end];
-    let i = start - 1;
-
-    for (let j = start; j < end; j++) {
-        if (arr[j] < pivot) {
-            i++;
-
-            swap(arr, i, j);
+            } else {
+                animation.push({indices:[pivot, j], sorted: true})
+            }
         }
-    }
-    swap(arr, i + 1, end);
-
-    return i + 1;
-}
-
-let currCall = 0;
-let partitionObjArr = [];
-
-function captureQuickSort(arr, left = 0, right = arr.length - 1) {
-
-    currCall++;
-    console.log(`[${currCall}] quickSort([${arr}], left(${left}), right(${right})) >> [${arr.slice(left, right+1)}]`);
-
-    // Get the partition information
-    const partitionObj = {};
-    partitionObj.partition = arr.slice(left, right+1);
-
-    if (partitionObj.partition.length > 0){
-        partitionObj.pivot = partitionObj.partition[partitionObj.partition.length-1];
-        partitionObj.level = currCall;
-        partitionObjArr.push(partitionObj);
-    }
-
-    if (left < right) {
-
-        // Get the partition information
-        let pivotIdx = capturePartition(arr, left, right);
-
-        // Recursive call to quicksort for left partition
-        captureQuickSort(arr, left, pivotIdx - 1);
-        // Recursive call to quicksort for right partition
-        captureQuickSort(arr, pivotIdx + 1, right);
-    }
-
-    // Decrement the level counter
-    currCall--;
-    return arr;
-}
-
-// Functions to get and clear the partition object array
-const getPartitionObjArray = () => { return partitionObjArr; };
-const clearPartitionObjArray = () => { partitionObjArr = []; };
-
-
-// Partition function
-function capturePartition(arr, start, end) {
-    
-    let pivot = arr[end];
-    let i = start - 1;
-    console.log(`partition([${arr}], ${start}, ${end}) >> [${arr.slice(start, end+1)}] >> pivot = ${pivot}`);
-
-    for (let j = start; j < end; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr, i, j);
+        i++
+        let temp = array[pivot]
+        array[pivot] = array[i]
+        array[i] = temp
+        if (i!==pivot) {
+            animation.push({indices:[pivot, i], sorted: i===pivot})
         }
+
+        return i//as new pivot
     }
-    swap(arr, i + 1, end);
-
-    return i + 1;
 }
-
-function swap(arr, i, j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-
-export { quickSort, captureQuickSort, getPartitionObjArray, clearPartitionObjArray };
-
